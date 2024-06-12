5a6c298312cf12fb60674fe9829c1d1df5e0dc95
Sudhanshu tiwari <sudhanshutiwari264@gmail.com>
2024-06-12 17:55:07 +0530

    added the debug module .env and also tested using the postman

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require('../models/owner-model')
 
 router.get('/',(req, res)=>{
   res.send("hey it's working");
 })
 
+if(process.env.NODE_ENV === 'development'){
+  router.post('/create',async function(req, res){
+    let owners = await ownerModel.find();
+    if(owners.length > 0){
+      return res
+      .status(500)
+      .send('You dont have permission to create a new owner.');
+    }
+    let {fullname, email, password} = req.body
+    let createdOwner = await ownerModel.create({
+      fullname,
+      email,
+      password
+    })
+    res.status(202).send(createdOwner);
+
+
+  });
+}
+
+console.log(process.env.NODE_ENV)
 module.exports = router;
